180dc8c34bdf112425c781d221ea6975ff8b53dd
Sheheryar Nadeem <Sheheryar.n@aimviz.com>
2018-03-06 11:57:11 +0500

    pull

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -89,5 +89,29 @@
       <script src="{{ asset('public/assets/js/customnew.js') }}"></script>
 
       <script src="{{ asset('public/assets/js/custom.js') }}" data-turbolinks-track="true"></script>
+
+      <script type="text/javascript">
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+      </script>
    </body>
 </html>

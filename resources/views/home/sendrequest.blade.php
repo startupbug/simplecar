a0a0cadbe4e11a51071af6b7e4aa6977df1358e1
Sheheryar Nadeem <Sheheryar.n@aimviz.com>
2018-03-07 13:02:20 +0500

    sad

--- resources/views/home/sendrequest.blade.php
+++ resources/views/home/sendrequest.blade.php
@@ -14,19 +14,23 @@
   </ul>
 
   <form class="simple_form new_order_form" id="new_order_form" action="{{route('submit_req')}}" accept-charset="UTF-8" method="post">
-    <section id='content1' class="hidden">
+    <section id='content1' class="">
        <div class='container container-center'>
           <div class='row row-eq-height'>
              <div class='col-md-6'>
                 <div class='config-wrapper'>
                    <div class='galleries'>
-                      <div class='carousel slide' data-ride='carousel' id='carousel-cars-79103'>
+                      <div class='carousel slide' data-ride='carousel' id=''>
                          <ol class='carousel-indicators'>
                             <li class='active' data-slide-to='0' data-target='#carousel-cars-79103'></li>
                          </ol>
                          <div class='carousel-inner' role='listbox'>
                             <div class='active item'>
-                               <img src="{{ asset('public/assets/images/uploads/41gO474olhL.jpg') }}" alt="41go474olhl" />
+                              @if(isset($car->car_image))
+                                 <img src="{{asset('/public/dashboard/img/car_assets/'.$car->car_image)}}" alt="" />
+                              @endif
+
+
                             </div>
                          </div>
                          <a class='left carousel-control' data-slide='prev' href='#carousel-cars-79103' role='button'>
@@ -316,13 +320,13 @@
           </div>
        </div>
     </section>
-    <section id="content2" class="">
+    <section id="content2" class="hidden">
        <div class="container container-center">
           <div class="row row-eq-height">
              <div class="col-md-6">
                 <div class="gallery">
                     @if(isset($car->car_image))
-                       <img class="img-responsive" src="{{asset('/public/dashboard/img/car_assets/'.$car->car_image)}}" alt="" />
+                       <img class="img-responsive" style="margin: auto;" src="{{asset('/public/dashboard/img/car_assets/'.$car->car_image)}}" alt="" />
                     @endif
                 </div>
                 <div class="specification">
@@ -382,7 +386,6 @@
                    </div>
                    <div class="bottom">
                       <div class="row">
-
                          <!--<<div class="col-sm-6">
                             <div class="specific-detail">
                                <div class="detail-name">
@@ -405,7 +408,6 @@
                                </div>
                             </div>
                          </div>-->
-
                       </div>
                    </div>
                 </div>
@@ -432,11 +434,11 @@
                          <input type="hidden" name="_token" value="{{Session::token()}}" />
 
                          @if(Auth::check())
-                            <input type="submit" name="commit" value="Get best offers" class="btn btn-lg btn-block" data-disable-with="Get best offers">                         
+                            <input type="submit" name="commit" value="Get best offers" class="btn btn-lg btn-block" data-disable-with="Get best offers">
                          @else
                             <a href="{{route('register_index')}}"><button type="button" name="commit" value="Please Login to Send Request" class="btn btn-lg btn-block" data-disable-with="Get best offers"> Please Login to Send Request</a>
                          @endif
-                         
+
 
                       </form>
                    </div>

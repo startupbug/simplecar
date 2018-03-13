79edfae7709688fe0f68dab42269fce269e9406d
aliaimviz <im.irfanmumtaz21@gmail.com>
2018-03-13 12:37:32 +0500

    checking

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -69,10 +69,6 @@
            </div>
         </div>
       </div>
-      <script id="IntercomSettingsScriptTag">
-        window.intercomSettings = {"app_id":"twdf3efd"};(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/twdf3efd';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})()
-      </script>
-
       <!-- jQuery -->
       <!-- <script src="{{ asset('public/assets/js/jquery.min.js') }}"></script> -->
 <script src="https://code.jquery.com/jquery-1.12.4.js" charset="utf-8"></script>
